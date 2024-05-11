e622958c08683cf40db51c086ef6d6f61dae746d
demo <yrt6keo@protonmail.com>
2024-05-11 20:23:35 +0800

    修改DockerBuild.sh尝试修正错误

--- DockerBuild.sh
+++ DockerBuild.sh
@@ -4,13 +4,15 @@ if [ ! -d jdk-21.0.3+9 ]; then
   wget https://aka.ms/download-jdk/microsoft-jdk-21.0.3-linux-x64.tar.gz && \
   tar -zxvf microsoft-jdk-21.0.3-linux-x64.tar.gz
 fi
-export JAVA_HOME=`pwd jdk-21.0.3+9`/jdk-21.0.3+9  && chmod 777 ./mvbw && \
+export JAVA_HOME=`pwd jdk-21.0.3+9`/jdk-21.0.3+9  && chmod 777 ./mvnw && \
 ./mvnw clean package && \
 cp target/student-score-0.0.1-SNAPSHOT.jar DockerBuild/student-score.jar && \
 cd DockerBuild && \
 sudo docker build -t student_score_java . && \
 echo "尝试删除已存在的运行中的同名Docker容器"
 sudo docker rm -f student_score_java
+# 判断如果镜像存在就运行镜像
+sudo docker image inspect student_score_java &> /dev/null && \
 echo "运行docker容器：" && \
 sudo docker run -d -p 8080:8080 --name student_score_java student_score_java && \
 echo "容器已运行在 localhost:8080"
